770458f8a7faffbbf6e142d098b8abc3d65d23ce
Jorge x <watikku@hotmail.com>
2022-02-24 19:47:51 +0100

    primer código añadido

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aluNombre = new System.Windows.Forms.TextBox();
+            this.listaAlumnos = new System.Windows.Forms.TextBox();
+            this.aluNota = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // aluNombre
+            // 
+            this.aluNombre.Location = new System.Drawing.Point(141, 91);
+            this.aluNombre.Name = "aluNombre";
+            this.aluNombre.Size = new System.Drawing.Size(186, 20);
+            this.aluNombre.TabIndex = 0;
+            // 
+            // listaAlumnos
+            // 
+            this.listaAlumnos.Location = new System.Drawing.Point(141, 144);
+            this.listaAlumnos.Multiline = true;
+            this.listaAlumnos.Name = "listaAlumnos";
+            this.listaAlumnos.ReadOnly = true;
+            this.listaAlumnos.Size = new System.Drawing.Size(187, 213);
+            this.listaAlumnos.TabIndex = 1;
+            // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(379, 91);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(56, 20);
+            this.aluNota.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 114);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Lista de alumnos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(376, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.aluNota);
+            this.Controls.Add(this.listaAlumnos);
+            this.Controls.Add(this.aluNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox aluNombre;
+        private System.Windows.Forms.TextBox listaAlumnos;
+        private System.Windows.Forms.TextBox aluNota;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
